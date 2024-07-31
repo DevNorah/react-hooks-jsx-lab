6b88e2ffc90fb2c71adabd439ead942be18dbbd9
norah seini kinyamasyo <norahkinyamasyo@moringaschool.com>
2024-08-01 01:47:34 +0300

    complete assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>She enjoys coding and baking.</p>
+    <img src={image} alt="I made this" />
+    </div>
+  )
 }
 
-export default About;
+export default About
+
